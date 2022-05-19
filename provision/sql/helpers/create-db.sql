b77fbc685355360a9b85f9e9774be65178ec0d93
rackdon <r.martinez@ride-on.es>
2022-05-19 11:25:02 +0200

    Add docker-compose and provision for database

---
+++ provision/sql/helpers/create-db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE sample_project
+WITH OWNER = owner
+     TEMPLATE = template1
+     ENCODING = 'UTF8'
+     LC_COLLATE = 'en_US.utf8'
+     LC_CTYPE = 'en_US.utf8';
+
+CREATE DATABASE sample_project_test
+WITH OWNER = owner
+     TEMPLATE = template1
+     ENCODING = 'UTF8'
+     LC_COLLATE = 'en_US.utf8'
+     LC_CTYPE = 'en_US.utf8';
