9e2709c1191192cfe79f1cce24149b074f1ffbd3
rackdon <r.martinez@ride-on.es>
2022-05-23 17:35:19 +0200

    Fix factory results

--- src/integrationTests/utils/factory.ts
+++ src/integrationTests/utils/factory.ts
@@ -12,6 +12,18 @@ export class Factory {
   private camelToSnakeCase(str: string): string {
     return str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`)
   }
+  private toCamel(str: string): string {
+    return str.replace(/([-_][a-z])/gi, ($1) =>
+      $1.toUpperCase().replace('_', '')
+    )
+  }
+
+  private parseKeys<T>(obj: Record<string, any>): T {
+    return Object.keys(obj).reduce((acc: Record<string, any>, k: string) => {
+      acc[this.toCamel(k)] = obj[k]
+      return acc
+    }, {}) as T
+  }
 
   private async insert(
     tableName: string,
@@ -34,6 +46,7 @@ export class Factory {
   }
 
   async insertUser(user?: User): Promise<User> {
-    return this.insert('users', user || generateUser())
+    const insertedUser = await this.insert('users', user || generateUser())
+    return this.parseKeys<User>(insertedUser)
   }
 }
